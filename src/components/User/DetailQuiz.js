a1eb462815a644c1cc7d7539cb65d9f85a94cf88
TTanh <tuananhtrieu2k5@gmail.com>
2024-01-27 19:47:51 +0700

    Process question data

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiServices";
+import _ from "lodash";
 
 const DetailQuiz = (props) => {
   const param = useParams();
@@ -13,6 +14,28 @@ const DetailQuiz = (props) => {
   const fetchQuestions = async () => {
     let res = await getDataQuiz(quizID);
     console.log(res);
+    if (res && res.EC === 0) {
+      let raw = res.DT;
+      let data = _.chain(raw)
+        // Group the elements of Array based on `color` property
+        .groupBy("id")
+        // `key` is group's name (color), `value` is the array of objects
+        .map((value, key) => {
+          let answers = [];
+          let questionDescription,
+            image = null;
+          value.forEach((item, index) => {
+            if (index === 0) {
+              questionDescription = item.description;
+              image = item.image;
+            }
+            answers.push(item.answers);
+          });
+          return { questionID: key, answers, questionDescription, image };
+        })
+        .value();
+      console.log(data);
+    }
   };
 
   return <div className="detail-quiz-container">Detail Quiz</div>;
